0508fbf16897879d203f3ce9df71406368db0416
takuya kashimura <takuya kashimura>
2017-11-05 18:31:33 -0500

    Create a simple client sample code.

---
+++ client/src/consts.js
@@ -0,0 +1,7 @@
+module.exports = function(){
+    return {
+        BASE_URL: 'https://mit-public.cloudhub.io/coursecatalog/v2/',
+        CLIENT_ID: '',
+        CLIENT_SECRET: ''
+    };
+};
---
+++ client/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // メインのJS
+    entry: "./src/main.js",
+    output: {
+      filename: "build/bundle.js"
+    },
+    watch: true
+  }
