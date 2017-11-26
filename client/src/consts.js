b4a30d210951c880b7b61c651e22978cb80bde03
takuya kashimura <taku2017@mit.edu>
2017-11-26 17:23:49 -0500

    Update to retrieve registered classes from the usersubject table

--- client/src/consts.js
+++ client/src/consts.js
@@ -1,7 +1,6 @@
-module.exports = function(){
-    return {
-        BASE_URL: 'https://mit-public.cloudhub.io/coursecatalog/v2/',
-        CLIENT_ID: '',
-        CLIENT_SECRET: ''
-    };
+module.exports = {
+    BASE_URL: 'https://mit-public.cloudhub.io/coursecatalog/v2/',
+    CLIENT_ID: '',
+    CLIENT_SECRET: '',
+    DEFAULT_CURRENT_TERM: '2017FA'    
 };
