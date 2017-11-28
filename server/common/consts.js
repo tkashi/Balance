0d3f1e6264d80a9daca5c36d6c29c5c398649484
takuya kashimura <takuya kashimura>
2017-11-28 15:34:01 -0500

    Chaange folder name 'libs' to 'plugins'

--- server/common/consts.js
+++ server/common/consts.js
@@ -1,7 +1,7 @@
 module.exports = {
     DUMMY_USER_ID: 'dummy_user_id',
 
-    DB_HOST: 'localhost',
+    DB_HOST: 'ec2-184-72-209-108.compute-1.amazonaws.com',
 
     DEFAULT_CURRENT_TERM: '2017FA'
 };
