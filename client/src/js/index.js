352d926f47046b526635fb147d26d621f695e38f
takuya kashimura <taku2017@mit.edu>
2017-11-30 08:53:40 -0500

    Integrate task list module to mockup

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -55,4 +55,48 @@ $(function() {
         rotate: 180,
         animate: { duration: 2e3, enabled: !0 }
     });
+
+    var taskLogic = {
+        __name: 'balance.desktop.TaskLogic',        
+    };
+
+    var taskController = {
+        __name: 'balance.desktop.TaskController',
+
+        // _formController: h5.ui.FormController,
+
+        // __meta: {
+        //     _formController: {
+        //         rootElement: '#register_task'                
+        //     }
+        // },
+
+        __ready: function(context) {
+            this.$find('.date').text(new Date().toDateString());
+            h5.ajax('../task/list', {
+                dataType: 'json'
+            }).done(res => {
+                this.$find('.task-num').text(res.length);
+                this.view.update('.list-group', 'tasks', {
+                    tasks: res.slice(0, 5)
+                });
+            });
+        },
+
+        '#register_task button click': function(context, $el) {
+            context.event.preventDefault();
+            var values = this._formController.getValue();
+            h5.ajax('task/register', {
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(values)
+            }).done(res => {
+                alert('Registered a task');
+            });
+        }
+    }
+
+    $(function() {
+        h5.core.controller('.page-wrapper', taskController);
+    });
 });
