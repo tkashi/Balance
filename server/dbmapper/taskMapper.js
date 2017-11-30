4186f4a185f5ce0a053d2189d428bf50152fe816
takuya kashimura <takuya kashimura>
2017-11-30 08:53:40 -0500

    Integrate task list module to mockup

--- server/dbmapper/taskMapper.js
+++ server/dbmapper/taskMapper.js
@@ -3,7 +3,28 @@ var driver = require('./driverFactory').createDriver();
 module.exports = {
     find: (query, callback) => {
         var collection = driver.db.collection('task');
-        collection.find(query).toArray(callback);
+        collection.find(query).toArray((error, tasks) => {
+            var ids = tasks.map(item => {
+                return item.subjectId
+            });
+            driver.db.collection('subject').find({
+                id: {
+                    '$in': ids
+                }
+            }).toArray((err, subjects) => {
+                tasks.forEach(task => {
+                    if (task.subjectId) {
+                        for (var i = 0, len = subjects.length; i < len; i++) {
+                            if (task.subjectId == subjects[i].id) {
+                                task.subject = subjects[i].title;
+                                return;
+                            }
+                        }
+                    }
+                });
+                callback(error || err, tasks);
+            });
+        });
     },
     
     findById: (id, callback) => {
