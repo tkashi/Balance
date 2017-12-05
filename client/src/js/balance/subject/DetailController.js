1b5eebfd773f5b368d166b68d4adf21b9968e005
柏村 拓哉 <taku2017@mit.edu>
2017-12-05 17:13:22 -0500

    Delete unexcuted codes

--- client/src/js/balance/subject/DetailController.js
+++ client/src/js/balance/subject/DetailController.js
@@ -37,47 +37,7 @@
                 }
                 this.view.update('.container-fluid', 'detail', tasksGroupByCategory);
             });
-        },
-
-        _appendSubject: function(subject) {
-            this.view.append('.table tbody', 'subject', {
-                subject: subject
-            });
-        },
-
-        _setLogisticsTab: function(type, list) {
-            this['_' + type + 's'] = list;
-            this._setLogistics(list[0], type, this.$find('#' + type));
-            
-            var length = list.length;
-            if (length > 1) {
-                this.$find('.' + type +'-tab').hide();
-                var $dropdown = this.$find('.' + type + '-tab-dropdown');
-                $dropdown.show();
-                var $dropdownMenu = $dropdown.find('.dropdown-menu');
-                var $li = $dropdownMenu.find('li').eq(0);
-                $dropdownMenu.empty();
-                $dropdownMenu.append($li);
-                for (var i = 1; i < length; i++) {
-                    var $cloneLi = $li.clone();
-                    $cloneLi.children().eq(0).text(type + ' ' + (i+1));
-                    $dropdownMenu.append($cloneLi);
-                }
-            }
-        },
-
-        _setLogistics: function(logistics, type, $form) {
-            var $checks = $form.find('[name=' + type +'Day]');
-            $checks.prop('checked', false);
-            var days = logistics.day.split('');
-            for (var i = 0, len = days.length; i < len; i++) {
-                $checks.filter('[value=' + days[i] + ']').prop('checked', true);
-            }
-            $form.find('[name=' + type + 'StartTime]').val(logistics.startTime);
-            $form.find('[name=' + type + 'EndTime]').val(logistics.endTime);    
-            $form.find('[name=' + type + 'Place]').val(logistics.place);    
-        },
-
+        }
     };
 
     h5.core.expose(detailController);
