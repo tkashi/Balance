57373100d8e1178545d281759a39ed5ffcef4ac1
Takuya Kashimura <taku2017@mit.edu>
2017-12-09 10:16:27 -0500

    Change data type

--- java/src/main/java/schedule/MongoDBClient.java
+++ java/src/main/java/schedule/MongoDBClient.java
@@ -4,7 +4,6 @@ import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import com.mongodb.MongoClient;
@@ -19,15 +18,6 @@ public class MongoDBClient {
 
     private static String HOST_NAME = "localhost";
     private static String DB_NAME = "test";
-
-
-    // private static Calendar BASE_DATE;
-
-    // static {
-    //     SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd");
-    //     BASE_DATE = Calendar.getInstance();
-    //     BASE_DATE.setTime(sdFormat.parse("2017/09/06"));
-    // }
 
     private static MongoClient mongoClient;
     private static MongoDatabase database;
@@ -48,13 +38,10 @@ public class MongoDBClient {
     public List<Task> readTasks(int planHorizon) throws Exception {
         MongoCollection<Document> collection = database.getCollection("task");
 
-        Calendar today = Calendar.getInstance();
-        today.set(Calendar.HOUR_OF_DAY, 0);
-
         Bson query = and(eq("type", "incoming"));
         List<Task> tasks = new ArrayList<>();
-        for (Document doc: collection.find()) {
-            tasks.add(new Task((int)((Double)doc.get("_id") * 1), (int)((Double)doc.get("duration")*1), (int)((Double)doc.get("dueDate")*1), (int)((Double)doc.get("urgency")*1), (int)((Double)doc.get("importance")*1)));
+        for (Document doc: collection.find(query)) {
+            tasks.add(new Task((Integer)doc.get("_id"), (Integer)doc.get("duration"), (Integer)doc.get("dueDateTime"), (Integer)doc.get("urgency"), (Integer)doc.get("importance")));
         }
         return tasks;
     }
